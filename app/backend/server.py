3b626d38cdde7ac60b057cbfbc8b0a65ae81718d
Joshua Pooranmal <jpooranm@umd.edu>
2020-05-02 21:39:59 -0400

    adding what's done so far for backend flask api

--- app/backend/server.py
+++ app/backend/server.py
@@ -3,7 +3,7 @@ import os
 
 from flask import Flask, request, jsonify
 
-from ingest import ingest, update, delete
+from ingest import get_files, send_to_indexer, update, delete
 from dir_manage import create_workspace, delete_workspace
 from search import search_from_strs
 
@@ -52,7 +52,62 @@ def delete_workspace_ep(guid):
         response = "DELETE WORKSPACE FAILED"
     return jsonify(response)
 
+## May need to decode paths here.
+@app.route('/import/<path>/<type>/<guid>', methods=['GET'])
+def import_ep(path, type, guid):
+    response = None
+    try:
+        response = get_files(path=path, import_type=type, workspace_guid=guid)
+    except:
+        response = "IMPORT FAILED"
+    return jsonify(response)
 
+## Subsequent call after import
+## Should receive JSON as a POST request argument
+@app.route('/index/<operation>/<guid>', methods=['GET', 'POST'])
+def index_ep(operation, guid):
+    response = None
+    try:
+        json_lst = request.get_json()
+        if operation == 'add':
+            response = send_to_indexer(json_lst=json_lst, workspace_guid=guid)
+        elif operation == 'update':
+            response = update(json_lst=json_lst, workspace_guid=guid)
+        elif operation =='delete':
+            response = delete(json_lst=json_lst, workspace_guid=guid)
+        else:
+            response = "INVALID INDEX OPERATION"
+    except:
+        response = "IMPORT FAILED"
+    return jsonify(response)
+
+## Search request
+## Should receive JSON as a POST request argument
+## Schema: 
+# {
+#     searchQuery: string
+#     dil: [startDil, endDil]
+#     dik: [startDik, endDik]
+#     dla: [startDla, endDla]
+#     mediaQuery: string
+#     options: [array of selected options]
+# }
+@app.route('/search/<guid>', methods=['GET', 'POST'])
+def search_ep(guid):
+    response = None
+    try:
+        json = request.get_json()
+        response = search_from_strs(
+            search_text=json['searchQuery'],
+            leg_datetime_range=json['dil'],
+            kive_datetime_range=json['dik'],
+            la_datetime_range=json['dla'], 
+            media_text_lst=json['mediaQuery'], 
+            fields_lst=json['options'], 
+            workspace_guid=guid)
+    except:
+        response = "IMPORT FAILED"
+    return jsonify(response)
 
 # Runs main (should stay at bottom)
 if __name__ == '__main__':

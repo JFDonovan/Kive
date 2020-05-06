47f63e6fa4648838fb03a38e39248b70e82dcb35
James Donovan <jfdonovan100@gmail.com>
2020-05-05 23:38:04 -0400

    Security.js added for encrypting and decrypting on the front end Fernet added to package.json test button removed from html

---
+++ app/js/security.js
@@ -0,0 +1,30 @@
+
+
+function getToken() {
+    let fernet = require('fernet');
+    let fs = remote.require('fs');
+
+    let path = appDataPath + "/secret_key";
+
+    fs.readFile(path, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        let secret = new fernet.Secret(data);
+        console.log(secret);
+        let token = new fernet.Token({
+            secret: secret
+            //time: Date.parse(1),
+            //iv: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        });
+        return token;
+    });
+}
+
+function encryptRequest(message) {
+    token.encode(message);
+}
+
+function decryptResponse(message) {
+    token.decode(message);
+}
